988a2081d8ac756b91a15d6e798641f7802c2926
Nagoor27 <babu.nagoor@mahindra.com>
2021-07-28 15:05:47 +0530

    Project initial setup

--- app/src/main/java/com/mfcwl/app/yms/Test.kt
+++ app/src/main/java/com/mfcwl/app/yms/Test.kt
@@ -5,5 +5,7 @@ import com.mfcwl.app.yms.platform.BaseActivity
 class Test : BaseActivity() {
     override fun getLayoutId(): Int {
         TODO("Not yet implemented")
+
+//        Comment for Base Activity
     }
 }
